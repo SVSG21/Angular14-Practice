34824e13f704138a310537149141da4f516c18fc
svsg21 <gauravacerpc@gmail.com>
2024-02-18 20:47:28 +0530

    Books Component is created and interpolation is practiced

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent {
+src1:string="https://m.media-amazon.com/images/I/71nAPgrH3aL._SY466_.jpg";
+src1_book_name:string="The Compound Effect";
+src1_author_name:string="Darren Hardy";
+price1:number=313;
+}
